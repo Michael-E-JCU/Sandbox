271ba120bae7d3d7147d458d5011a2a98b193671
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-11 00:43:41 +1100

    Add use of history with append

--- rent_bills_etc/doing_the_stuff.py
+++ rent_bills_etc/doing_the_stuff.py
@@ -1,6 +1,14 @@
+import datetime
+
+
 def main():
     print_menu()
     response = get_response()
+    menu(response)
+
+
+def menu(response):
+    date = datetime.datetime.now().strftime("%c")
     if response == "S":
         setup_thingy()
         print("Setup complete!")
@@ -11,11 +19,14 @@ def main():
         main()
 
     elif response == "C":
-        calucumalate()
+        calucumalate(date)
         main()
 
     elif response == "P":
-        pay_bills()
+        pay_bills(date)
+        main()
+    elif response == "H":
+        print_history()
         main()
     else:
         quit()
@@ -27,17 +38,20 @@ def print_menu():
 (C)alucumalate
 (O)wed
 (P)ay
+(H)istory
 (Q)uit""")
 
 
 def validate_response(response):
-    if response != "S" and response != "C" and response != "Q" and response != "O" and response != "P":
+    if response != "S" and response != "C" and response != "Q" and response != "O" and response != "P" and \
+            response != "H":
         return False
     return True
 
 
 def print_owed():
     in_file = open("owed.txt", "r")
+    print("\n")
     print(f"Money owed: ${in_file.readline()}")
     in_file.close()
 
@@ -51,9 +65,9 @@ def setup_thingy():
     out_file.close()
 
 
-def calucumalate():
+def calucumalate(date):
     owed_file = open("owed.txt", "r")
-    history = open("history.txt", "r+")
+    history = open("history.txt", "a")
     electricity = float(input("Electricity : $"))
     water = float(input("Water + Hot water : $"))
     gas = float(input("Enter gas : $"))
@@ -65,21 +79,36 @@ def calucumalate():
     owed_file = open("owed.txt", "w")
     owed_file.flush()
     print(total, file=owed_file)
-    history.readlines(-1)
     owed_file.close()
+    print("\n")
+    print(f"On the {date}\n${total} was owed.")
+    print(f"On the {date}\n${total} was owed.\n", file=history)
     history.close()
 
 
-def pay_bills():
+def pay_bills(date):
     owed_file = open("owed.txt", "r")
     owed = float(owed_file.readline())
     owed_file.close()
     paid = float(input("Enter amount paid: $"))
     total = owed - paid
+    owed = total
     owed_file = open("owed.txt", "w")
+    history = open("history.txt", "a")
+    print("\n")
+    print(f"{date}\n${paid} was paid, leaving ${owed} owed.")
+    print(f"\n{date}\n${paid} was paid, leaving ${owed} owed.", file=history)
+    history.close()
     owed_file.flush()
     print(total, file=owed_file)
     owed_file.close()
+
+
+def print_history():
+    history = open("history.txt", "r")
+    for lines in history:
+        print(lines)
+    history.close()
 
 
 def get_response():
@@ -88,6 +117,14 @@ def get_response():
         print("Invalid response!")
         response = str(input(">>> ")).upper()
     return response
+
+
+def get_names():
+    names = []
+    in_file = open("name.txt", "r")
+    for lines in in_file:
+        names.append(lines)
+    return names
 
 
 main()
