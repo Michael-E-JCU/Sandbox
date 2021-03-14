55380916dbee2c3f61efaecef9243b969d13bff9
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-15 10:54:23 +1100

    Add changes to main menu, etc.

--- Flashcards/flashcards.py
+++ Flashcards/flashcards.py
@@ -1,34 +1,35 @@
+""" Flashcards"""
+
 import random
 
 
 def main():
     print_menu()
-    response = str(input("Enter response: ")).lower()
+    response = input("Enter response: ").lower()
     while not validate_response(response):
         response = str(input("Enter response: ")).lower()
     question_list = get_questions()
     answer_list = get_answers()
-    run_menu_item(response, question_list, answer_list)
-
-
-def run_menu_item(response, question_list, answer_list):
-    if response == "a":
-        print("In progress")
-    if response == "p":
-        complete_question(question_list, answer_list)
-        main()
-    if response == "q":
-        quit()
-    if response == "i":
-        print_instructions()
-        main()
+    while response != "q":
+        if response == "a":
+            print("In progress")
+            print_menu()
+            response = input("Enter response: ").lower()
+        if response == "p":
+            complete_question(question_list, answer_list)
+            print_menu()
+            response = input("Enter response: ").lower()
+        if response == "i":
+            print_instructions()
+            print_menu()
+            response = input("Enter response: ").lower()
 
 
 def get_questions():  # Imports the text, and then for each line in the text, adds that to list and returns list.
     in_file = open("questions.txt", "r")
     question_list = []
-    for question in in_file:
-        question_list.append(question.strip("\n"))
+    for line in in_file:
+        question_list.append(line.strip("\n"))
     in_file.close()
     return question_list
 
@@ -36,20 +37,20 @@ def get_questions():  # Imports the text, and then for each line in the text, ad
 def get_answers():  # Imports the text, and then for each line in the text, adds that to list and returns list.
     in_file = open("answers.txt", "r")
     answer_list = []
-    for answer in in_file:
-        answer_list.append(answer.strip("\n"))
+    for line in in_file:
+        answer_list.append(line.strip("\n"))
     in_file.close()
     return answer_list
 
 
 def complete_question(question_list, answer_list):  # This is the function for completing the question.
-    question = get_random_question(question_list, answer_list)
+    question = generate_random_question(question_list, answer_list)
     print(question[0])  # Prints the question
     answer = str(input("Enter response here: ")).capitalize()  # Gets the user answer to above question
     while not return_to_menu(answer):
         check = validate_answer(answer, question)  # Checks if the answer is correct
         print(check)  # Prints if the answer is correct or not
-        question = get_random_question(question_list, answer_list)
+        question = generate_random_question(question_list, answer_list)
         print(question[0])  # Prints the question
         answer = str(input("Enter response here: ")).capitalize()  # Gets the user answer to above question
 
@@ -88,7 +89,7 @@ def validate_response(response):  # Checks if the response is in the list of res
     return True
 
 
-def get_random_question(question_list, answer_list):  # Uses the len of the list to generate a random question
+def generate_random_question(question_list, answer_list):  # Uses the len of the list to generate a random question
     number_items_in_list = len(question_list)
     number = random.randint(0, number_items_in_list - 1)
     question = question_list[number]
