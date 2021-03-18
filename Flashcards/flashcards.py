ff423194ad96cfb9d2aa11918a1e2a5f0c83efea
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-18 20:09:04 +1100

    fix: add variable, change in_files lists

--- Flashcards/flashcards.py
+++ Flashcards/flashcards.py
@@ -13,7 +13,7 @@ def main():
     response = input("Enter response: ").lower()
     question_list = get_questions()
     answer_list = get_answers()
-    while response != "q":
+    while response != "q":  # While loop for resposnes
         if response == "a":
             print("In progress")
             print_menu()
@@ -33,27 +33,26 @@ def main():
 
 
 def get_questions():  # Imports the text, and then for each line in the text, adds that to list and returns list.
-    question_list = list(open("questions.txt", "r"))
+    question_list = [line.strip("\n") for line in open("questions.txt", "r")]  # Strips \n from each line
     return question_list
 
 
 def get_answers():  # Imports the text, and then for each line in the text, adds that to list and returns list.
-    answer_list = list(open("answers.txt", "r"))
+    answer_list = [line.strip("\n") for line in open("answers.txt", "r")]  # Strips \n from each line
     return answer_list
 
 
 def complete_question(question_list, answer_list):  # This is the function for completing the question.
-    questions_answers_list = generate_random_question(question_list, answer_list)
-    correct_answer = questions_answers_list[1]
-    question = questions_answers_list[0]  # List has both answer and question in it.
+    question, answer = generate_random_question(question_list, answer_list)  # Generates random question
     print(question)  # Prints the question from the question/answer list.
-    response = str(input("Enter response here: ")).capitalize()  # Gets the user answer to above question
+    response = str(input("Enter response here: ")).lower().capitalize()  # Gets the user answer to above question
     while not return_to_menu(response):
-        answer = validate_answer(response, correct_answer)  # Checks if the answer is correct
-        print(answer)  # Prints if the answer is correct or not
-        questions_answers_list = generate_random_question(question_list, answer_list)
-        print(questions_answers_list[0])  # Prints the question
-        response = str(input("Enter response here: ")).capitalize()  # Gets the user answer to above question
+        print(answer)
+        validate_response = validate_answer(response, answer)  # Checks if the answer is correct
+        print(validate_response)  # Prints if the answer is correct or not
+        question, answer = generate_random_question(question_list, answer_list)
+        print(question)  # Prints the question
+        response = str(input("Enter response here: ")).lower().capitalize()  # Gets the user answer to above question
 
 
 def return_to_menu(response):
@@ -70,11 +69,11 @@ def print_menu():
 (A)ccuracy
 (Q)uit\n""")
 
-# test
-def validate_answer(response, correct_answer):
-    if response != correct_answer:
-        return f"False! The correct answer is {correct_answer}\n"
-    return f"Correct! {correct_answer}\n"
+
+def validate_answer(response, answer):
+    if response != answer:
+        return f"False! The correct answer is {answer}\n"
+    return f"Correct! {answer}\n"
 
 
 def generate_random_question(question_list, answer_list):  # Uses the len of the list to generate a random question
